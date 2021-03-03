b43552432947e26cbec978b9b1d86968ed4ceeff
vladimir_grigoriev <vladimir_grigoriev@mail.ru>
2021-03-04 00:19:35 +0300

    fixed bug in src/items/management/commands/add_items.py: now it is creating and updating data with manage.py add_items

--- src/items/management/commands/add_items.py
+++ src/items/management/commands/add_items.py
@@ -4,33 +4,30 @@ from django.conf import settings
 import requests
 
 
-URL = 'https://raw.githubusercontent.com/stepik-a-w/drf-project-boxes/master/foodboxes.json'
+URL = "https://raw.githubusercontent.com/stepik-a-w/drf-project-boxes/master/foodboxes.json"
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **kwargs):
         response = requests.get(url=URL).json()
-        
-        for item in response:
 
-            obj, created = Item.objects.get_or_create(
-                id=item['id'],
+        for item in response:
+            obj, created = Item.objects.update_or_create(
+                id=item["id"],
                 defaults={
-                    'title': item['title'],
-                    'description': item['description'],
-                    'weight': item['weight_grams'],
-                    'price': item['price'],
-                    'image': item['image']
-                }
+                    "title": item["title"],
+                    "description": item["description"],
+                    "weight": item["weight_grams"],
+                    "price": item["price"],
+                },
             )
             if created:
                 url = item["image"]
-                filename = url.split('/')[-1]
+                filename = url.split("/")[-1]
                 r = requests.get(url)
-                with open(str(settings.MEDIA_ROOT) + '/' + filename, mode='wb') as f:
+                with open(str(settings.MEDIA_ROOT) + "/" + filename, mode="wb") as f:
                     f.write(r.content)
                 obj.image = filename
                 obj.save()
-        
+
         return
